4e9c61220df7871065b3c71dc013ec8b76346c5f
Baker1773 <baker.1774@gmail.com>
2016-08-31 15:47:34 -0400

    Player Mouse Controlle
    Player is now controlled with mouse

--- Pong/Pong/Assets/Scripts/PlayerMovementScript.cs
+++ Pong/Pong/Assets/Scripts/PlayerMovementScript.cs
@@ -11,9 +11,12 @@ public class PlayerMovementScript : MonoBehaviour {
         WallLimit = 4f;
         PaddleSize = 0.25f;
     }
-	
-	// Update is called once per frame
-	void FixedUpdate () {
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        /**
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
@@ -24,6 +27,19 @@ public class PlayerMovementScript : MonoBehaviour {
         Vector3 pos = transform.position;
         pos = newPos(pos, movem);
         transform.position = pos;
+        **/
+
+        Vector3 pos = transform.position;
+
+        RaycastHit rch;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out rch))
+        {
+            Vector3 movem = new Vector3(0f, rch.point.y, rch.point.z);
+            pos = newPos2(pos, movem);
+        }
+        transform.position = pos;
+
     }
 
     private Vector3 newPos(Vector3 pos, Vector3 movem)
@@ -32,6 +48,16 @@ public class PlayerMovementScript : MonoBehaviour {
         x = pos.x;
         y = Mathf.Max(Mathf.Min(pos.y + movem.y, WallLimit - PaddleSize), -WallLimit + PaddleSize);
         z = Mathf.Max(Mathf.Min(pos.z + movem.z, WallLimit - PaddleSize), -WallLimit + PaddleSize);
+
+        return (new Vector3(x, y, z));
+    }
+
+    private Vector3 newPos2(Vector3 pos, Vector3 movem)
+    {
+        float x, y, z;
+        x = pos.x;
+        y = Mathf.Max(Mathf.Min(movem.y, WallLimit - PaddleSize), -WallLimit + PaddleSize);
+        z = Mathf.Max(Mathf.Min(movem.z, WallLimit - PaddleSize), -WallLimit + PaddleSize);
 
         return (new Vector3(x, y, z));
     }

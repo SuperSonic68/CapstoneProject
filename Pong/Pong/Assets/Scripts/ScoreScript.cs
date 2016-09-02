da17e5676bf5c209805de012e91ce97c8fe8188a
SuperSonic68 <coyle.79@osu.edu>
2016-09-02 12:12:11 -0400

    Pausing, Scene switching, Winning

--- Pong/Pong/Assets/Scripts/ScoreScript.cs
+++ Pong/Pong/Assets/Scripts/ScoreScript.cs
@@ -2,11 +2,14 @@
 using System.Collections;
 using UnityEngine.UI;
 using System;
+using UnityEngine.SceneManagement;
 
 public class ScoreScript : MonoBehaviour {
 
     public Text Player_Score;
     public Text NPC_Score;
+	public Text WinText;
+	public string MenuSceneName;
 
     private int playerScore, npcScore;
 
@@ -22,8 +25,8 @@ public class ScoreScript : MonoBehaviour {
 
     private void UpdateScore()
     {
-        Player_Score.text = "Score: " + playerScore.ToString();
-        NPC_Score.text = "Score: " + npcScore.ToString();
+        Player_Score.text = "Player 1: " + playerScore.ToString();
+        NPC_Score.text = "Player 2: " + npcScore.ToString();
     }
 
     void FixedUpdate()
@@ -37,6 +40,17 @@ public class ScoreScript : MonoBehaviour {
                 UpdateScore();
                 GetComponent<GoalScript>().Goal();
                 Scored = true;
+
+				if (npcScore > 4) 
+				{
+					WinText.color = Color.blue;
+					WinText.text = "Player 2 WINS!!";
+
+
+					StartCoroutine(Wait ());
+
+				}
+
             }
             else if (x < -20)
             {
@@ -44,7 +58,25 @@ public class ScoreScript : MonoBehaviour {
                 UpdateScore();
                 GetComponent<GoalScript>().Goal();
                 Scored = true;
+
+				if (playerScore > 4) 
+				{
+					WinText.color = Color.red;
+					WinText.text = "Player 1 WINS!!";
+
+
+					StartCoroutine(Wait ());
+				}
+
             }
         }
     }
+
+	IEnumerator Wait()
+	{
+		Time.timeScale = 0.0f;
+		yield return new WaitForSeconds (3);
+		SceneManager.LoadScene (MenuSceneName);
+	}
+
 }
